74a8b67d0401f19b504f2f5d0bc7247feb19257a
ManN <dubkois@users.noreply.github.com>
2016-11-26 10:10:18 +0100

    Corrected cube primitive so that its left side is correctly expressed for face culling.

--- mecacellviewer/primitives/cube.hpp
+++ mecacellviewer/primitives/cube.hpp
@@ -18,7 +18,7 @@ struct Cube {
 	const std::vector<float> normals = {-1.0, -1.0, 1.0,  1.0, -1.0, 1.0,  1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,
 	                                    -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0};
 	const std::vector<unsigned int> indices = {0, 1, 2, 2, 3, 0, 3, 2, 6, 6, 7, 3, 7, 6, 5, 5, 4, 7,
-	                                          4, 0, 3, 3, 7, 4, 0, 1, 5, 5, 4, 0, 1, 5, 6, 6, 2, 1};
+	                                          4, 0, 3, 3, 7, 4, 4, 5, 1, 1, 0, 4, 1, 5, 6, 6, 2, 1};
 	Cube() {}
 	void load(QOpenGLShaderProgram &shader) {
 		shader.bind();
